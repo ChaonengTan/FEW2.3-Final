0a4cbea8ef1b04fe4222acbd9d9f624da01f64b2
ChaonengTan <chaonengtan@gmail.com>
2021-07-13 23:52:51 -0700

    *

--- src/components/Starwars.js
+++ src/components/Starwars.js
@@ -9,20 +9,20 @@ function Starwars() {
             <div className='charCard'>
                 <div>
                     <h2>Character Details</h2>
-                    <div>Name: {data.name}</div>
-                    <div>Year: {data.birth_year}</div>
-                    <div>Height: {data.height}</div>
-                    <div>Gender: {data.gender}</div>
-                    <div>Eye Color: {data.eye_color}</div>
+                    <div>Name: {char.name}</div>
+                    <div>Year: {char.birth_year}</div>
+                    <div>Height: {char.height}</div>
+                    <div>Gender: {char.gender}</div>
+                    <div>Eye Color: {char.eye_color}</div>
                 </div>
                 <div>
                     <h2>Homeworld Details</h2>
-                    <div>Planet: {data.homeworld.name}</div>
-                    <div>Climate: {data.homeworld.climate}</div>
+                    <div>Planet: {char.homeworld.name}</div>
+                    <div>Climate: {char.homeworld.climate}</div>
                 </div>
                 <div>
                     <h2>Films</h2>
-                    {data.films.map(film => {
+                    {char.films.map(film => {
                         return(
                             <div>
                                 {film.title}
